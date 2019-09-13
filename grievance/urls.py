34946813517db7a8613879361acc4a97aae6dd7b
kestal-lab <shanumittal147@gmail.com>
2019-09-13 22:19:33 +0530

    updated models and calling method from url

--- grievance/urls.py
+++ grievance/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.conf.urls import url
-import grievance.views as VIEWS
+from django.urls import path,include
+import grievance.tempViews as VIEWS
 
 app_name = 'grievance'
 urlpatterns =[
 	# path('', VIEWS.HomeView.as_view())
 	path('cmo/', VIEWS.CMO.as_view()),
+	path('accounts/', include('django.contrib.auth.urls')),
 ]

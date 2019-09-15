c9063d9120947348f031ee65330c776855954fe7
kestal-lab <shanumittal147@gmail.com>
2019-09-15 19:43:01 +0530

    student back-end and front-end corrected

--- grievance/views/studentHomeView.py
+++ grievance/views/studentHomeView.py
@@ -4,107 +4,96 @@ from grievance.forms import StudentHomeViewForm, ApplicationStatusForm
 import datetime
 from . import constants as constants
 from django.views import generic
+from django.http import HttpResponseRedirect
 
 class studentHomeView(generic.TemplateView):
+
+    def redirect(self):
+        return HttpResponseRedirect('/ps-grievance/redirect')
+
     def get(self, request,*args, **kwargs):
         current_user=request.user
         user = UserProfile.objects.get(user=current_user)
-        # user = UserProfile.objects.get(user=User.objects.get(username="20160080G"))
-        # const= str(constants.Status.NOAPPLICATION.value())
-        const = "0"
+        const = constants.Status.NOAPPLICATION.value
         attempt_status=[const,const,const]
         description=["","",""]
         comments=["","",""]
         newStation=["","",""]
-        if (GrievanceForm.objects.filter(student_id = user).count())!=0:
-            formEntry = GrievanceForm.objects.get(student_id = user)
-            i=0
-            applicationstatus_list = ApplicationStatus.objects.filter(student_id = user)
-            for x in applicationstatus_list:
-                attempt_status[x.attempt-1]=x.status
-                comments[x.attempt-1]=x.level2Comment
-                newStation[x.attempt-1]=x.newStation
-                i+=1
-            details={       #things to be passed to front end
-            'formEntry':formEntry,
-            'attemptStatus':attempt_status,
-            'descriptions' : description,
-            'campus':user.campus,
-            'name': user.name,
-            'comments':comments,
-            'newStation':newStation
-            }
-            return render (request, "grievance/grievanceForm.html", details)
-            #TODO : return render
-        else:
-            attempt_status = [const,const,const]
-            return render (request, "grievance/grievanceForm.html", {'attemptStatus':attempt_status,'name':user.name,'campus':user.campus,'descriptions':description,'comments':comments,
-            'newStation':newStation})
 
+        formEntry = GrievanceForm.objects.get(student_id = user)
+        applicationstatus_list = ApplicationStatus.objects.filter(student_id = user)
+        for x in applicationstatus_list:
+            attempt_status[x.attempt-1]=x.status-1
+            comments[x.attempt-1]=x.level2Comment
+            newStation[x.attempt-1]=x.newStation
+            description[x.attempt-1]=x.description
+
+        details={       #things to be passed to front end
+        'formEntry':formEntry,
+        'attemptStatus':attempt_status,
+        'descriptions' : description,
+        'campus':user.campus,
+        'name': user.name,
+        'comments':comments,
+        'newStation':newStation
+        }
+        return render (request, "grievance/grievanceForm.html", details)
+       
     def post(self, request, *args, **kwargs):
-        # current_user=request.user
-        # user = UserProfile.objects.get(user=current_user)
-        user = UserProfile.objects.get(user=User.objects.get(username="20160080G"))
+        current_user=request.user
+        user = UserProfile.objects.get(user=current_user)
         if request.POST.get("submit1"):
             if (GrievanceForm.objects.filter(student_id = user).count())==0:
-                # formEntry = GrievanceForm.objects.create(student_id=user)
-                # applicationStatus = ApplicationStatus.objects.create(student_id = user)
-                temp = StudentHomeViewForm(request.POST, request.FILES)
-                temp2 = ApplicationStatusForm(request.POST)
-                form1=temp.save(commit=False)
-                form2=temp2.save(commit=False)
+                form1 = StudentHomeViewForm(request.POST, request.FILES).save(commit=False)
+                form2 = ApplicationStatusForm(request.POST).save(commit=False)
                 form1.student_id=user
-                form2.student_id=user
                 form1.applicationDate = datetime.datetime.now()
-                form1.priority = 1#constants.priority.LOW.value()
+                form1.priority = constants.priority.LOW.value
                 form1.save()
-                form2.status = 1#constants.Status.INPROGRESS.value()
+                form2.student_id=user
+                form2.status = constants.Status.INPROGRESS.value
                 form2.attempt = 1
                 form2.level = 1
                 form2.lastChangedDate = datetime.datetime.now()
                 form2.campus = user.campus
                 form2.natureOfQuery = form1.natureOfQuery
                 form2.save()
-                #return render
+                return self.redirect()
             else:
-                #redirect
-                print("")
+                return self.redirect()
         elif request.POST.get("submit2"):
             # formEntry = GrievanceForm.objects.get(user = user)
             # applicationStatus = ApplicationStatus.objects.get(student_id = user)
             # formEntry = GrievanceForm.objects.get(user = user)
-            applicationStatus = ApplicationStatus.objects.get(student_id = user)
-            if(applicationStatus.attempt==1):
-                applicationStatus.attempt = 2
-                applicationStatus.level = 2           
-                applicationStatus.status = 1     
-                applicationStatus.lastChangedDate = datetime.datetime.now()
-                form2 = ApplicationStatusForm(request.POST, instance = applicationStatus)
-                form2.save() 
-                #return render
+            applicationStatus_object = ApplicationStatus.objects.filter(student_id = user)
+            if(len(applicationStatus_object) == 1):
+                form = ApplicationStatusForm(request.POST).save(commit=False)
+                form.student_id = user
+                form.status = constants.Status.INPROGRESS.value
+                form.attempt = 2
+                form.level = 2
+                form.lastChangedDate = datetime.datetime.now()
+                form.campus = user.campus
+                form.natureOfQuery = applicationStatus_object[0].natureOfQuery
+                form.save()
+                return self.redirect()
             else:   
-                #redirect
-                print("") 
+                return self.redirect() 
         elif request.POST.get("submit3"):
            # formEntry = GrievanceForm.objects.get(user = user)
            # applicationStatus = ApplicationStatus.objects.get(student_id = user)
         #    formEntry = GrievanceForm.get(user = user)
-           applicationStatus = ApplicationStatus.objects.get(student_id = user)
-           if(applicationStatus.attempt==2):
-               applicationStatus.attempt = 3
-               applicationStatus.level = 2
-               applicationStatus.status = 1                
-               applicationStatus.lastChangedDate = datetime.datetime.now()
-               form2 = ApplicationStatusForm(request.POST, instance = applicationStatus)
-               form2.save()
-               #return render
-
-
-                
-
-
-
-
-
-         
-
+            applicationStatus_object = ApplicationStatus.objects.filter(student_id = user)
+            if(len(applicationStatus_object) == 2):
+                form = ApplicationStatusForm(request.POST).save(commit=False)
+                form.student_id = user
+                form.status = constants.Status.INPROGRESS.value
+                form.attempt = 3
+                form.level = 2
+                form.lastChangedDate = datetime.datetime.now()
+                form.campus = user.campus
+                form.natureOfQuery = applicationStatus_object[0].natureOfQuery
+                form.save()
+                return self.redirect()
+            else:   
+                return self.redirect() 

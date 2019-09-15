eaa2fd90e7c695eb5eac6fd31ea66474d570c7fa
chetan22 <ck.chetan20@gmail.com>
2019-09-16 01:37:31 +0530

    Update script

--- grievance/management/commands/populateTestUsers.py
+++ grievance/management/commands/populateTestUsers.py
@@ -1,20 +1,22 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from django.utils.crypto import get_random_string
 from grievance.models import *
 import datetime
 
 class Command(BaseCommand):
   def _create(self):
     for i in range(10):
-      (user, created) = User.objects.get_or_create(username="user"+str(i),password="123456")
+      password = get_random_string(8)
+      (user, created) = User.objects.get_or_create(username="user"+str(i),password=password)
       # user=User.objects.get(username="user"+str(i))
       (_, _) = UserProfile.objects.get_or_create(user = user, token=0, name="Name "+str(i), contact="9876543210", email="user@mail.com", campus=0)
 
-    (user, created) = User.objects.get_or_create(username="cmo",password="123456")
+    (user, created) = User.objects.get_or_create(username="cmo",password=password)
     # user=User.objects.get(username="cmo")
     (_, _) = UserProfile.objects.get_or_create(user = user, token=1, name="CMO "+str(i), contact="9876543210", email="cmo@mail.com", campus=0)
   
-    (user, created) = User.objects.get_or_create(username="ad",password="123456")
+    (user, created) = User.objects.get_or_create(username="ad",password=password)
     # user=User.objects.get(username="ad")
     (_, _) = UserProfile.objects.get_or_create(user = user, token=2, name="AD "+str(i), contact="9876543210", email="ad@mail.com", campus=0)
 
@@ -55,6 +57,8 @@ class Command(BaseCommand):
                                               level=1, status=0, description="Hello",
                                               campus=0, natureOfQuery=0)
 
+
+    print(password)
 
   def handle(self, *args, **options):
     self._create()

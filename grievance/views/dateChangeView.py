f3cff8a94b1219b9703cef91753148986b55b742
chetan22 <ck.chetan20@gmail.com>
2019-09-16 01:03:11 +0530

    Minor changes in dateChangeView

--- grievance/views/dateChangeView.py
+++ grievance/views/dateChangeView.py
@@ -7,8 +7,11 @@ BASE_DIR = settings.BASE_DIR
 
 class updateLastSubmissionDate(generic.TemplateView):
 
+	template_name = 'grievance/date.html'
+
 	def post(self, request, *args, **kwargs):
 		newDate = request.POST.get("date")
 		jsFile = os.path.join(BASE_DIR, 'grievance/static/grievance/js/date.js')
 		with open(jsFile, mode='w') as file:
 			file.write("var date=\""+newDate+"\";")
+		

8240e2bcc8d2ea353597cbbbd8301e75036d253b
kestal-lab <shanumittal147@gmail.com>
2019-10-24 18:02:13 +0530

    add script to clear models

---
+++ grievance/management/commands/clearModels.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from grievance.models import *
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+	def _create(self):
+		GrievanceForm.objects.all().delete()
+		UserProfile.objects.all().delete()
+		ApplicationStatus.objects.all().delete()
+		InformativeQuerryForm.objects.all().delete()
+		User.objects.all().exclude(is_staff=True, is_superuser=True).delete()
+
+	def handle(self, *args, **options):
+		self._create()

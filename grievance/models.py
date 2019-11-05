fa9a0b18abf4126a21bb36e5393041ff59c9bb6f
2018A7PS0396G <f20180396@goa.bits-pilani.ac.in>
2019-11-05 21:42:25 +0530

    .

--- grievance/models.py
+++ grievance/models.py
@@ -61,7 +61,7 @@ class GrievanceForm(models.Model):
 	document4 = models.FileField(upload_to=path_and_rename,validators=[FileExtensionValidator(allowed_extensions=['pdf']),validate_document], blank=True)
 	document5 = models.FileField(upload_to=path_and_rename,validators=[FileExtensionValidator(allowed_extensions=['pdf']),validate_document], blank=True)
 	priority = models.IntegerField(default=0)
-	preferenceNumberOfAllocatedStation = models.IntegerField(blank = True) #TODO remove blank true
+	preferenceNumberOfAllocatedStation = models.IntegerField(default = 2) #TODO remove blank true
 
 	def __str__(self):
 		return str(self.student_id)

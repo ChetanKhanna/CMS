c41f3b7ca3db612836bd74fea1f0b01ee3047cbe
chetan22 <ck.chetan20@gmail.com>
2020-05-07 23:01:06 +0530

    Changes security settings

--- ps_grievance/settings.py
+++ ps_grievance/settings.py
@@ -130,14 +130,7 @@ STATI_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com' # mail service smtp
-EMAIL_HOST_USER = 'developers.psd@gmail.com' # email id
-EMAIL_HOST_PASSWORD = 'Q!W@E#R$' # password
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
